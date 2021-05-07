6256545a2bd5c1dea48ab301f40ca1da6d53a0d1
danclouston <danclouston@gmail.com>
2021-05-07 11:26:30 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,54 @@
 
-var fruitArray = ["Banana", "Blueberry", "Peach", "Apple", "Lemon", "Mango", "Raspberry"]
-
-var fruitInFridge = false
-while (fruitInFridge==false) {
-  var fruitGuess = prompt("Enter a fruit")
-  if (fruitArray.includes(fruitGuess)) {
-    alert(`Good guess! ${fruitGuess} is in/on my fridge`)
-    fruitInFridge = true
+var fruitArray = [
+  {
+    name: "banana",
+    number: 1,
+    image: "images/banana.jpg"
+  },
+  {
+    name: "cucumber",
+    number: 2,
+    image: "images/cucumber.gif"
+  },
+  {
+    name: "apple",
+    number: 4,
+    image: "images/apple.jpg"
   }
-  else {
+]
+
+// Start the loop with false guess
+var fruitInFridge = false
+
+// Keep asking user to enter fruit until they find a match
+while (fruitInFridge==false) {
+
+  // Ask the user to enter a fruit
+  var fruitGuess = prompt("Enter a fruit")
+  fruitGuess = fruitGuess.toLowerCase()
+
+  // Loop through the objects in fruitArray
+  for (i=0; i<fruitArray.length; i++) {
+
+    // Find an object that matches the user fruitGuess
+    if (fruitArray[i].name.includes(fruitGuess)) {
+      var idx = i
+      fruitInFridge = true
+    }
+  }
+
+  // No match was found
+  if (fruitInFridge==false) {
     alert(`${fruitGuess} is not in/on my fridge. Try again!`)
   }
 }
+
+// Found a match (idx = matching array index --> points to object)
+alert(`Good guess! ${fruitGuess} is in/on my fridge`)
+document.write(`There are ${fruitArray[idx].number} ${fruitArray[idx].name} in my fridge`)
+
+// Style the image directly with JS
+document.body.style.backgroundImage = `url(${fruitArray[idx].image})`
+document.body.style.backgroundRepeat = "no-repeat"
+document.body.style.backgroundPosition = "center"
+document.body.style.backgroundSize = "50%"
